4988ec2131866698fa3a849e6efe157309a825d5
sahong.pak <sahong.pak@gmail.com>
2024-05-06 01:29:31 +0900

    :lipstick: Make HeaderView.vue UI

--- jwt-vue/src/views/includes/HeaderView.vue
+++ jwt-vue/src/views/includes/HeaderView.vue
@@ -1,7 +1,11 @@
 <script setup>
+    import MenuComponent from '@/components/headers/MenuComponent.vue'
 </script>
 
 <template>
+    <v-app-bar>
+        <MenuComponent />
+    </v-app-bar>
 </template>
 
 <style scoped>
