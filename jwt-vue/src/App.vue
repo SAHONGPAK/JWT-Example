35b39f2ae5c9fa1d37271a350be324fa41b90b5e
sahong.pak <sahong.pak@gmail.com>
2024-05-06 01:27:00 +0900

    :lipstick: Make App.vue UI

--- jwt-vue/src/App.vue
+++ jwt-vue/src/App.vue
@@ -1,7 +1,14 @@
 <script setup>
+    import { RouterView } from 'vue-router'
+    import HeaderView from '@/views/includes/HeaderView.vue'
 </script>
 
 <template>
+    <v-layout>
+        <HeaderView />
+
+        <RouterView />
+    </v-layout>
 </template>
 
 <style scoped>
