1239e5c2cc9ed213e62451167d8af6d710cfd611
sahong.pak <sahong.pak@gmail.com>
2024-05-06 01:49:00 +0900

    :lipstick: Make MenuComponent.vue UI

--- jwt-vue/src/components/headers/MenuComponent.vue
+++ jwt-vue/src/components/headers/MenuComponent.vue
@@ -1,7 +1,43 @@
 <script setup>
+    import { ref } from 'vue'
+    import { RouterLink } from 'vue-router'
+
+    const menuList = ref([
+        {name: "로그인", routeName: "login"},
+        {name: "회원가입", routeName: "signUp"},
+        {name: "게시판", routeName: "board"},
+        {name: "마이페이지", routeName: "myPage"},
+        {name: "로그아웃", routeName: "logout"},
+    ])
+
+    const logout = async () => {
+
+    }
+
 </script>
 
 <template>
+    <v-app-bar-nav-icon icon="mdi-menu" />
+
+    <v-app-bar-title
+        text="JWT Example"
+        class="v-col-3"
+    />
+
+    <v-list class="v-col-8">
+        <v-row class="justify-end">
+            <template v-for="menu in menuList" :key="menu.name">
+                <v-list-item>
+                    <template v-if="menu.routeName === 'logout'">
+                        <RouterLink to="/" @click.prevent="logout">{{ menu.name }}</RouterLink>
+                    </template>
+                    <template v-else>
+                        <RouterLink :to="{name: menu.routeName }">{{ menu.name }}</RouterLink>
+                    </template>
+                </v-list-item>
+            </template>
+        </v-row>
+    </v-list>
 </template>
 
 <style scoped>
