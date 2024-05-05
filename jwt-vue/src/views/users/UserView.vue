df3620e7f821524249cbde9fb86ee141a853228e
sahong.pak <sahong.pak@gmail.com>
2024-05-06 01:53:47 +0900

    :lipstick: Make UserView.vue UI

--- jwt-vue/src/views/users/UserView.vue
+++ jwt-vue/src/views/users/UserView.vue
@@ -1,7 +1,13 @@
 <script setup>
+    import { RouterView } from 'vue-router'
 </script>
 
 <template>
+    <v-main>
+        <v-container>
+            <RouterView />
+        </v-container>
+    </v-main>
 </template>
 
 <style scoped>
