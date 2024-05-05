45597453884e55ec0c686049afdefdeed5088189
sahong.pak <sahong.pak@gmail.com>
2024-05-06 01:49:35 +0900

    :hammer: Connect Components

--- jwt-vue/src/router/index.js
+++ jwt-vue/src/router/index.js
@@ -1,6 +1,15 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import MainView from '@/views/MainView.vue'
 
+import UserView from '@/views/users/UserView.vue'
+import LoginComponent from '@/components/users/LoginComponent.vue'
+import SignUpComponent from '@/components/users/SignUpComponent.vue'
+import MyPageComponent from '@/components/users/MyPageComponent.vue'
+
+import BoardView from '@/views/boards/BoardView.vue'
+
+
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
@@ -8,6 +17,33 @@ const router = createRouter({
       path: '/',
       name: 'main',
       component: MainView
+    },
+    {
+      path: '/user',
+      name: 'user',
+      component: UserView,
+      children: [
+        {
+          path: 'login',
+          name: 'login',
+          component: LoginComponent
+        },
+        {
+          path: 'signUp',
+          name: 'signUp',
+          component: SignUpComponent
+        },
+        {
+          path: 'myPage',
+          name: 'myPage',
+          component: MyPageComponent
+        }
+      ]
+    },
+    {
+      path: '/board',
+      name: 'board',
+      component: BoardView
     }
   ]
 })
