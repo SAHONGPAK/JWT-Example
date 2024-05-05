6627e1e8b1604f2b28676dcced4d2d56569a5383
sahong.pak <sahong.pak@gmail.com>
2024-05-06 03:10:24 +0900

    :hammer: Add CORS Config

--- jwt-springboot/jwt-springboot/src/main/java/com/example/jwt/config/CORSConfig.java
+++ jwt-springboot/jwt-springboot/src/main/java/com/example/jwt/config/CORSConfig.java
@@ -1,5 +1,22 @@
 package com.example.jwt.config;
 
-public class CORSConfig {
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@Configuration
+public class CORSConfig implements WebMvcConfigurer {
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		
+		registry
+			.addMapping("/**") // 허용하려는 API 요청 경로
+			.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+			.allowedOrigins("http://localhost:5173/")
+			.allowCredentials(true)
+			.exposedHeaders("Authorization")
+			.maxAge(3600);
+		
+		WebMvcConfigurer.super.addCorsMappings(registry);
+	}
 }
