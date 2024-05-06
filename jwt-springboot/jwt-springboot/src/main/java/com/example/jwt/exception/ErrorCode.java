5b674cbc4c5a2e31543f713c7bf357d960235c45
sahong.pak <sahong.pak@gmail.com>
2024-05-06 21:19:08 +0900

    :hammer: Add Refresh Token Expire Code

--- jwt-springboot/jwt-springboot/src/main/java/com/example/jwt/exception/ErrorCode.java
+++ jwt-springboot/jwt-springboot/src/main/java/com/example/jwt/exception/ErrorCode.java
@@ -3,6 +3,8 @@ package com.example.jwt.exception;
 import org.springframework.http.HttpStatus;
 
 public enum ErrorCode {
+	
+	REFRESH_TOKEN_EXPIRED(HttpStatus.UNAUTHORIZED.value(), "비정상 토큰입니다."),
 	NOT_FOUND_USER(HttpStatus.BAD_REQUEST.value(), "사용자를 찾을 수 없습니다.");
 	
 	private int httpStatus;
