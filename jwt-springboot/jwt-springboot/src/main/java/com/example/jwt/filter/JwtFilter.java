fb5b97dbc76a3d4df69024721af42cbbddc33adf
sahong.pak <sahong.pak@gmail.com>
2024-05-06 17:06:39 +0900

    :hammer: Preflight Headers and Exclude URI

--- jwt-springboot/jwt-springboot/src/main/java/com/example/jwt/filter/JwtFilter.java
+++ jwt-springboot/jwt-springboot/src/main/java/com/example/jwt/filter/JwtFilter.java
@@ -1,5 +1,50 @@
 package com.example.jwt.filter;
 
-public class JwtFilter {
+import java.io.IOException;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@Component
+public class JwtFilter extends OncePerRequestFilter {
+
+	@Override
+	protected boolean shouldNotFilter(HttpServletRequest httpServletRequest) throws ServletException {
+
+		String[] excludeURIList = {
+				"/auth/login", "/auth/signUp", "auth/logout",
+				"swagger-ui", "api-docs"
+		};
+		
+		for(String excludeURI : excludeURIList) {
+			if(excludeURI.equals(httpServletRequest.getRequestURI())) {
+				return true;
+			}
+		}
+		
+		return false;
+	}
+	
+	@Override
+	protected void doFilterInternal(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, FilterChain filterChain)
+			throws ServletException, IOException {
+		
+		if(httpServletRequest.getMethod().equals("OPTIONS")) {
+			httpServletResponse.setHeader("Access-Control-Allow-Credentials", "true");
+			httpServletResponse.setHeader("Access-Control-Allow-Headers", "Content-Type");
+			httpServletResponse.setHeader("Access-Control-Allow-Headers", "authorization");
+			httpServletResponse.setHeader("Access-Control-Allow-Origin", "http://localhost:5173");
+			httpServletResponse.setStatus(HttpStatus.OK.value());
+			
+			return;
+		}
+		
+		filterChain.doFilter(httpServletRequest, httpServletResponse);
+	}
 }
