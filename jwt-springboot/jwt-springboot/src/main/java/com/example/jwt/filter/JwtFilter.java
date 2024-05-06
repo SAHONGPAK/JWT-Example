1728034626c0cccd0d27e970a7ea1278816916b3
sahong.pak <sahong.pak@gmail.com>
2024-05-06 21:23:01 +0900

    :hammer: Add Refresh and AccessToken Validation

--- jwt-springboot/jwt-springboot/src/main/java/com/example/jwt/filter/JwtFilter.java
+++ jwt-springboot/jwt-springboot/src/main/java/com/example/jwt/filter/JwtFilter.java
@@ -1,10 +1,17 @@
 package com.example.jwt.filter;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
+
+import com.example.jwt.exception.ErrorCode;
+import com.example.jwt.exception.ErrorResponse;
+import com.example.jwt.service.AuthService;
+import com.example.jwt.util.HeaderUtil;
+import com.example.jwt.util.JwtUtil;
 
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
@@ -13,17 +20,24 @@ import jakarta.servlet.http.HttpServletResponse;
 
 @Component
 public class JwtFilter extends OncePerRequestFilter {
+	
+	private final JwtUtil jwtUtil;
+	private final AuthService authService;
+	
+	public JwtFilter(JwtUtil jwtUtil, AuthService authService ) {
+		this.jwtUtil = jwtUtil;
+		this.authService = authService;
+	}
 
 	@Override
 	protected boolean shouldNotFilter(HttpServletRequest httpServletRequest) throws ServletException {
-
 		String[] excludeURIList = {
 				"/auth/login", "/auth/signUp", "auth/logout",
 				"swagger-ui", "api-docs"
 		};
 		
 		for(String excludeURI : excludeURIList) {
-			if(excludeURI.equals(httpServletRequest.getRequestURI())) {
+			if(httpServletRequest.getRequestURI().contains(excludeURI)) {
 				return true;
 			}
 		}
@@ -34,16 +48,55 @@ public class JwtFilter extends OncePerRequestFilter {
 	@Override
 	protected void doFilterInternal(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, FilterChain filterChain)
 			throws ServletException, IOException {
-		
+
 		if(httpServletRequest.getMethod().equals("OPTIONS")) {
 			httpServletResponse.setHeader("Access-Control-Allow-Credentials", "true");
-			httpServletResponse.setHeader("Access-Control-Allow-Headers", "Content-Type");
-			httpServletResponse.setHeader("Access-Control-Allow-Headers", "authorization");
+			httpServletResponse.setHeader("Access-Control-Allow-Headers", "content-type, authorization");
 			httpServletResponse.setHeader("Access-Control-Allow-Origin", "http://localhost:5173");
 			httpServletResponse.setStatus(HttpStatus.OK.value());
 			
 			return;
 		}
+		
+		
+		
+		if(httpServletRequest.getRequestURI().contains("/auth/refresh")) {
+			
+			String refreshToken = HeaderUtil.getRefreshToken(httpServletRequest);
+			
+			if(refreshToken != null) {
+				
+				if(!jwtUtil.isValidToken(refreshToken, "RefreshToken") || !authService.isValidToken(refreshToken)) {
+					
+					httpServletResponse.setStatus(ErrorCode.REFRESH_TOKEN_EXPIRED.getHttpStatus());
+					httpServletResponse.setContentType("application/json;charset=UTF-8");
+					
+					ErrorResponse errorResponse = ErrorResponse.of(ErrorCode.REFRESH_TOKEN_EXPIRED);
+					
+					PrintWriter printWriter = httpServletResponse.getWriter();
+					printWriter.print(errorResponse.toString());
+					return;
+				}
+			}
+			
+		}
+		else {
+			
+			String accessToken = HeaderUtil.getAccessToken(httpServletRequest);
+			
+			if(accessToken == null || !jwtUtil.isValidToken(accessToken, "AccessToken") || !authService.isValidToken(accessToken)) {
+				httpServletResponse.setStatus(ErrorCode.REFRESH_TOKEN_EXPIRED.getHttpStatus());
+				httpServletResponse.setContentType("application/json;charset=UTF-8");
+				httpServletResponse.setHeader("Access-Control-Allow-Origin", "http://localhost:5173");
+				
+				ErrorResponse errorResponse = ErrorResponse.of(ErrorCode.REFRESH_TOKEN_EXPIRED);
+				
+				PrintWriter printWriter = httpServletResponse.getWriter();
+				printWriter.print(errorResponse.toString());
+				return;
+			}
+		}
+		
 		
 		filterChain.doFilter(httpServletRequest, httpServletResponse);
 	}
