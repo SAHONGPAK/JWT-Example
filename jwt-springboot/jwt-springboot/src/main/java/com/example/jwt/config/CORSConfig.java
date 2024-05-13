673b707699a5370a4228743ad14277b58cd1b037
sahong.pak <sahong.pak@gmail.com>
2024-05-13 17:52:50 +0900

    :bulb: Add CORSConfig.java Comments

--- jwt-springboot/jwt-springboot/src/main/java/com/example/jwt/config/CORSConfig.java
+++ jwt-springboot/jwt-springboot/src/main/java/com/example/jwt/config/CORSConfig.java
@@ -15,7 +15,7 @@ public class CORSConfig implements WebMvcConfigurer {
 			.allowedOrigins("http://localhost:5173/") // 허용하려는 클라이언트 측 주소
 			.allowCredentials(true) // HttpOnly Cookie를 사용하기 위한 설정
 			.exposedHeaders("Access-Control-Allow-Headers")
-			.exposedHeaders("Authorization")
+			.exposedHeaders("Authorization") // 클라이언트에서 해당 Header의 값을 확인할 수 있도록 설정.
 			.maxAge(3600); // Preflight Cache 설정
 		
 		WebMvcConfigurer.super.addCorsMappings(registry);
